f7f65a66b2bb19bd9163e57200ec307d42aa1ac0
XZFlip12 <oldfaqm@gmail.com>
2026-02-23 01:28:11 +0700

    second try

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,23 +1,26 @@
 from django.contrib import admin
+from .models import Post, Category, Location
+
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'pub_date', 'is_published')
     list_filter = ('is_published', 'pub_date')
     search_fields = ('title', 'text')
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'is_published')
     list_filter = ('is_published',)
     search_fields = ('title', 'description')
+
 
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_published')
     list_filter = ('is_published',)
     search_fields = ('name',)
 
-# Register your models here.
-from .models import Post, Category, Location
 
+# Register your models here.
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Location, LocationAdmin)
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,26 +1,27 @@
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
-
 from .models import Post, Category
 
 
 # Create your views here.
 def index(request):
     template = 'blog/index.html'
-    post_list = (Post.objects
-    .select_related(
-        'category'
+    post_list = (
+        Post.objects
+        .select_related(
+            'category'
+        )
+        .filter(
+            is_published=True,
+            pub_date__lte=timezone.now(),
+            category__is_published=True
+        )
+        .order_by(
+            'title'
+        )
+        .reverse()[:5]
     )
-    .filter(
-        is_published=True,
-        pub_date__lte=timezone.now(),
-        category__is_published=True
-    )
-    .order_by(
-        'title'
-    )
-    .reverse()[:5])
     context = {'post_list': post_list}
     return render(request, template, context)
 
@@ -28,7 +29,8 @@ def index(request):
 def post_detail(request, id):
     template = 'blog/detail.html'
     post = get_object_or_404(
-        Post.objects.select_related('category'),
+        Post.objects
+        .select_related('category'),
         Q(id=id),
         Q(is_published=True),
         Q(pub_date__lte=timezone.now()),
@@ -45,16 +47,20 @@ def category_posts(request, category_slug):
         slug=category_slug,
         is_published=True
     )
-    post_list = (Post.objects
-    .filter(
-        category=category,
-        is_published=True,
-        pub_date__lte=timezone.now(),
-        category__is_published=True
+    post_list = (
+        Post.objects
+        .filter(
+            category=category,
+            is_published=True,
+            pub_date__lte=timezone.now(),
+            category__is_published=True
+        )
+        .order_by(
+            'title'
+        )
     )
-    .order_by(
-        'title'
-    ))
-    context = {'category': category,
-               'post_list': post_list}
+    context = {
+        'category': category,
+        'post_list': post_list
+    }
     return render(request, template, context)
